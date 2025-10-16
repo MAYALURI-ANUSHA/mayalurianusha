36cb8f7f02e017b3ae88620963436cfe2d84b412
MAYALURI ANUSHA <102647671+MAYALURI-ANUSHA@users.noreply.github.com>
2025-10-17 00:14:35 +0530

    Add files via upload

---
+++ forms/contact.php
@@ -0,0 +1,31 @@
+<?php
+$receiving_email_address = 'mayalurianusha@gmail.com';
+
+if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+} else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+}
+
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
+
+$contact->to = $receiving_email_address;
+$contact->from_name = $_POST['name'];
+$contact->from_email = $_POST['email'];
+$contact->subject = $_POST['subject'];
+
+// Optional: SMTP (recommended)
+$contact->smtp = array(
+  'host' => 'smtp.gmail.com',
+  'username' => 'yourgmail@gmail.com',
+  'password' => 'your-app-password', // use Gmail App Password
+  'port' => '587'
+);
+
+$contact->add_message( $_POST['name'], 'From');
+$contact->add_message( $_POST['email'], 'Email');
+$contact->add_message( $_POST['message'], 'Message', 10);
+
+echo $contact->send(); // returns "OK" on success
+?>
